3d6f92aab30e89c25165ce44316cb6a0f091deae
HebaZaki <h_borhan@hotmail.com>
2024-03-07 08:15:32 +0200

    base model class

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """base module"""
 import uuid
 import datetime
-
+from models import storage
 
 class BaseModel:
     """creating Base class"""
@@ -20,10 +20,12 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
+        storage.new(self)
 
     def save(self):
         """saves object into the database"""
         self.updated_at = datetime.datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns dict. containing all keys/values of __dict__ of instance"""
