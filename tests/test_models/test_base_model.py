fb273fa2fa86a8be03db58b770f6676a8b9f6edd
HebaZaki <h_borhan@hotmail.com>
2024-03-08 11:01:20 +0200

    Testing BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -24,9 +24,32 @@ class TestBase(unittest.TestCase):
         base_model = BaseModel(**data)
         self.assertIsInstance(base_model, BaseModel)
         self.assertEqual(base_model.id, '1')
-        self.assertEqual(base_model.created_at, datetime.datetime(2024, 3, 7, 12, 0))
-        self.assertEqual(base_model.updated_at, datetime.datetime(2024, 3, 7, 12, 0))
+        self.assertEqual(base_model.created_at, datetime.datetime(
+            2024, 3, 7, 12, 0))
+        self.assertEqual(base_model.updated_at, datetime.datetime(
+            2024, 3, 7, 12, 0))
         self.assertEqual(base_model.name, 'Test')
+
+    def test_to_dict(self):
+        """Test to_dict method"""
+        base_model = BaseModel()
+        base_model_dict = base_model.to_dict()
+
+        self.assertIsInstance(base_model_dict, dict)
+        self.assertIn('__class__', base_model_dict)
+        self.assertIn('id', base_model_dict)
+        self.assertIn('created_at', base_model_dict)
+        self.assertIn('updated_at', base_model_dict)
+
+    def test_str(self):
+        """Test __str__ method"""
+        base_model = BaseModel()
+        expected_output = "[{}] ({}) {}".format(
+            base_model.__class__.__name__,
+            base_model.id,
+            base_model.__dict__
+        )
+        self.assertEqual(str(base_model), expected_output)
 
 
 if __name__ == '__main__':
