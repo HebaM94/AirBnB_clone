001a47597f7aa9cbe6b8835f347b9a5218ab4e09
Heba <hebamagde@gmail.com>
2024-03-09 06:29:43 +0200

    7.create

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """console"""
 import cmd
+import sys
 from models.base_model import BaseModel
 import json
 
@@ -17,7 +18,34 @@ class HBNBCommand(cmd.Cmd):
         """Quit the hbnb shell"""
         print("")
         return True
+    
+    def do_creat(self, arg):
+        """Create a new object Class(arg)"""
+        if len(arg) == 0:
+            print("** class name missing **")
+            return
+        else:
+            class_name = arg.split()[0]
+            if class_name != "BaseModel":
+                print("** class doesn't exist **")
+                return
+            else:
+                new_obj = BaseModel()
+                new_obj.save()
+                print("{}".format(new_obj.id))
 
 
+    def do_show(self, arg):
+        """Show all objects of Class(arg)"""
+
+    def do_destroy(self, arg):
+        """"""
+
+    def do_all(self, arg):
+        """"""
+
+    def do_update(self, arg):
+        """"""
+    
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
