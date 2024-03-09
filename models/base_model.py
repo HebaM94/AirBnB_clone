3c8394e17968424d741057dbda7eb95320fb18ea
Heba <hebamagde@gmail.com>
2024-03-09 06:51:51 +0200

    update import storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,8 @@
 """base module"""
 import uuid
 import datetime
-import models
+from . import storage
+
 
 class BaseModel:
     """creating Base class"""
@@ -22,12 +23,12 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
-        models.storage.new(self)
+        storage.new(self)
 
     def save(self):
         """saves object into the database"""
         self.updated_at = datetime.datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """returns dict. containing all keys/values of __dict__ of instance"""
