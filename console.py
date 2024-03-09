efd77c97c999f2d6613d2fc191b17a6eea443aea
Heba <hebamagde@gmail.com>
2024-03-09 07:20:46 +0200

    update pycodestyle

--- console.py
+++ console.py
@@ -18,14 +18,15 @@ class HBNBCommand(cmd.Cmd):
         """Quit the hbnb shell"""
         print("")
         return True
-    
+
     def emptyline(self):
         """override the cmd emptyline function to do nothing
         if empty line and enter passed to console"""
         pass
 
     def do_create(self, arg):
-        """Create a new object Class(arg)"""
+        """Creates a new instance of BaseModel,
+        saves it (to the JSON file) and prints the id"""
         if len(arg) == 0:
             print("** class name missing **")
             return
@@ -39,18 +40,22 @@ class HBNBCommand(cmd.Cmd):
                 new_obj.save()
                 print("{}".format(new_obj.id))
 
-
     def do_show(self, arg):
-        """Show all objects of Class(arg)"""
+        """Prints the string representation of an instance
+        based on the class name and id"""
 
     def do_destroy(self, arg):
-        """"""
+        """Deletes an instance based on the class name
+        and id (save the change into the JSON file)"""
 
     def do_all(self, arg):
-        """"""
+        """Prints all string representation of all instances
+        based or not on the class name"""
 
     def do_update(self, arg):
-        """"""
+        """Updates an instance based on the class name and id
+        by adding or updating attribute (save the change into the JSON file)"""
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
