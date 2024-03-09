d0764a1cb3b01389a1dae98838f71993b8d26a0b
Heba <hebamagde@gmail.com>
2024-03-10 01:29:07 +0200

    update

--- models/base_model.py
+++ models/base_model.py
@@ -21,11 +21,11 @@ class BaseModel:
                     isinstance(kwargs['updated_at'], str):
                 self.updated_at = datetime.datetime.strptime(
                     self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
-            return
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-        models.storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+            models.storage.new(self)
 
     def save(self):
         """saves object into the database"""
