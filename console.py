8ee8a4f92125ea102222c400f8eceff8258b2b12
“HebaM94” <hebamagde@gmail.com>
2024-03-10 16:37:09 -0700

    update console

--- console.py
+++ console.py
@@ -64,7 +64,7 @@ class HBNBCommand(cmd.Cmd):
                 if key not in storage.all():
                     print("** no instance found **")
                     return
-                print(storage.all()[key])
+                print(str(storage.all()[key]))
 
     def do_destroy(self, arg):
         """Deletes an instance based on the class name
@@ -103,7 +103,7 @@ class HBNBCommand(cmd.Cmd):
                 obj_list.append(str(obj))
             elif obj.__class__.__name__ == args[0]:
                 obj_list.append(str(obj))
-            print(obj_list)
+        print(obj_list)
 
     def do_update(self, arg):
         """Updates an instance based on the class name and id
