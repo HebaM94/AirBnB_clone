c1babed3d5d378375130f2054899f22dd0e84ae5
Heba <hebamagde@gmail.com>
2024-03-10 09:54:12 +0200

    update reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -38,20 +38,9 @@ class FileStorage:
         """Deserialize the JSON file to __objects."""
         try:
             with open(self.__file_path, 'r', encoding="utf-8") as file:
-                new = file.read()
-                loaded_objects = json.loads(new)
+                loaded_objects = json.load(file)
                 for key, value in loaded_objects.items():
-                    #class_name, obj_id = key.split('.')
-                    # Convert datetime strings to datetime objects
-                    # value['created_at'] = datetime.datetime.strptime(
-                    #         value['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
-                    # value['updated_at'] = datetime.datetime.strptime(
-                    #         value['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
-                    # Recreate BaseModel objects
                     obj = BaseModel(**value)
-                    # Assign class name to obj
-                    # obj.__class__.__name__ = class_name
-                    # obj.id = obj_id  # Assign object ID to obj
                     self.new(obj)
         except FileNotFoundError:
             pass
