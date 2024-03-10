6df10abb15ed285c0839fd713d9fec8fad3ff8ac
Heba <hebamagde@gmail.com>
2024-03-10 11:01:39 +0200

    restore

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,31 +17,31 @@ class FileStorage:
 
     def all(self):
         """Return the dictionary __objects."""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """Set in __objects the obj with key <obj class name>.id"""
-        FileStorage.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
         """Serialize __objects to the JSON file."""
-        if len(FileStorage.__objects) > 0:
+        if len(self.__objects) > 0:
             new_dict = {}
-            for key, obj in FileStorage.__objects.items():
+            for key, obj in self.__objects.items():
                 new_dict[key] = obj.to_dict()
 
-        with open(FileStorage.__file_path, 'w', encoding="utf-8") as file:
+        with open(self.__file_path, 'w', encoding="utf-8") as file:
             file.write(json.dumps(new_dict, indent=4))
 
     def reload(self):
         """Deserialize the JSON file to __objects."""
         try:
-            with open(FileStorage.__file_path, 'r', encoding="utf-8") as file:
+            with open(self.__file_path, 'r', encoding="utf-8") as file:
                 new = file.read()
                 loaded_objects = json.loads(new)
                 for key, value in loaded_objects.items():
                     class_name = value.get('__class__')
-                    obj =BaseModel(**value)
-                    FileStorage.__objects[key] = obj
+                    obj = BaseModel(**value)
+                    self.__objects[key] = obj
         except FileNotFoundError:
             pass
