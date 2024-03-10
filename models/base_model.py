d9226addf74f953f8a9bd3b9efc28d19a0ecee1d
Heba <hebamagde@gmail.com>
2024-03-10 11:41:53 +0200

    update

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,13 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """defines all common attributes/methods for other classes"""
         if len(kwargs) > 0:
-            kwargs['created_at'] = datetime.datetime.strptime(
-                kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs['updated_at'] = datetime.datetime.strptime(
-                kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
+            self.created_at = datetime.datetime.strptime(
+                kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            self.updated_at = datetime.datetime.strptime(
+                kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
