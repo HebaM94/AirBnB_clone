835335acf4f171364961a84a9a83483d515d180a
Heba <hebamagde@gmail.com>
2024-03-11 07:28:35 +0200

    update

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -49,7 +49,8 @@ class TestFileStorage(unittest.TestCase):
         # Reload the data and check if the object is present
         new_storage = FileStorage()
         new_storage.reload()
-        self.assertIn("BaseModel." + obj.id, new_storage.all())
+        objs = FileStorage._FileStorage__objects
+        self.assertIn("BaseModel." + obj.id, objs)
 
     def test_save_with_arg(self):
         with self.assertRaises(TypeError):
