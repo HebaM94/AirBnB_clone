0207a9798985bb65aee71320bcd00ca1e73f2873
Heba <hebamagde@gmail.com>
2024-03-11 05:40:21 +0200

    update

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -11,6 +11,7 @@ class TestFileStorage(unittest.TestCase):
         """Set up test environment."""
         # Create an instance of FileStorage
         self.storage = FileStorage()
+        self.storage.__objects.clear()
 
     def tearDown(self):
         """Clean up after the test."""
