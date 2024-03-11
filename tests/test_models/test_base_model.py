502b8c4036a7e9ee0a7abc5e6e264a2eee1b1708
Heba <hebamagde@gmail.com>
2024-03-11 04:45:59 +0200

    update test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -78,8 +78,8 @@ class TestBase(unittest.TestCase):
     def test_update_attributes(self):
         """Test updating attributes"""
         base_model = BaseModel()
-        base_model.name = 'New Name'
-        self.assertEqual(base_model.name, 'New Name')
+        setattr(base_model, 'name', 'New Name')
+        self.assertEqual(getattr(base_model, 'name'), 'New Name')
 
     def test_save_method(self):
         """Test save method"""
@@ -92,8 +92,9 @@ class TestBase(unittest.TestCase):
     def test_created_at_updated_at(self):
         """Test created_at and updated_at attributes."""
         base_model = BaseModel()
-        self.assertIsInstance(base_model.created_at, datetime.datetime)
-        self.assertIsInstance(base_model.updated_at, datetime.datetime)
+        self.assertEqual(base_model.created_at, base_model.updated_at)
+        self.assertIsInstance(base_model.created_at, datetime.datetime.now())
+        self.assertIsInstance(base_model.updated_at, datetime.datetime.now())
 
     def test_save(self):
         """Test the save() method."""
