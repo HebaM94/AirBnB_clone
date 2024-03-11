190416d2759195e97b6de134dfe36621627311af
Heba <hebamagde@gmail.com>
2024-03-11 05:04:58 +0200

    update test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,12 @@
 import unittest
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 import os
 
 
@@ -24,28 +30,88 @@ class TestFileStorage(unittest.TestCase):
         self.assertEqual(len(self.storage.all()), 0)
 
         # Add an object
-        obj = BaseModel()
-        self.storage.new(obj)
+        base_model = BaseModel()
+        user = User()
+        state = State()
+        place = Place()
+        city = City()
+        amenity = Amenity()
+        review = Review()
+
+        self.storage.new(base_model)
+        self.storage.new(user)
+        self.storage.new(state)
+        self.storage.new(place)
+        self.storage.new(city)
+        self.storage.new(amenity)
+        self.storage.new(review)
+
         self.assertEqual(len(self.storage.all()), 1)
-        self.assertIn("BaseModel." + obj.id, self.storage.all())
+        self.assertIn("BaseModel." + base_model.id, self.storage.all())
+        self.assertIn("User." + user.id, self.storage.all())
+        self.assertIn("State." + state.id, self.storage.all())
+        self.assertIn("Place." + place.id, self.storage.all())
+        self.assertIn("city." + city.id, self.storage.all())
+        self.assertIn("Amenity." + amenity.id, self.storage.all())
+        self.assertIn("Review." + review.id, self.storage.all())
 
     def test_new(self):
         """Test the new() method."""
-        obj = BaseModel()
-        self.storage.new(obj)
-        self.assertIn("BaseModel." + obj.id, self.storage.all())
+        base_model = BaseModel()
+        user = User()
+        state = State()
+        place = Place()
+        city = City()
+        amenity = Amenity()
+        review = Review()
+
+        self.storage.new(base_model)
+        self.storage.new(user)
+        self.storage.new(state)
+        self.storage.new(place)
+        self.storage.new(city)
+        self.storage.new(amenity)
+        self.storage.new(review)
+
+        self.assertIn("BaseModel." + base_model.id, self.storage.all())
+        self.assertIn("User." + user.id, self.storage.all())
+        self.assertIn("State." + state.id, self.storage.all())
+        self.assertIn("Place." + place.id, self.storage.all())
+        self.assertIn("city." + city.id, self.storage.all())
+        self.assertIn("Amenity." + amenity.id, self.storage.all())
+        self.assertIn("Review." + review.id, self.storage.all())
 
     def test_save_reload(self):
         """Test the save() and reload() methods."""
         # Add an object and save it
-        obj = BaseModel()
-        self.storage.new(obj)
+        base_model = BaseModel()
+        user = User()
+        state = State()
+        place = Place()
+        city = City()
+        amenity = Amenity()
+        review = Review()
+
+        self.storage.new(base_model)
+        self.storage.new(user)
+        self.storage.new(state)
+        self.storage.new(place)
+        self.storage.new(city)
+        self.storage.new(amenity)
+        self.storage.new(review)
+
         self.storage.save()
 
         # Reload the data and check if the object is present
         new_storage = FileStorage()
         new_storage.reload()
-        self.assertIn("BaseModel." + obj.id, new_storage.all())
+        self.assertIn("BaseModel." + base_model.id, self.storage.all())
+        self.assertIn("User." + user.id, self.storage.all())
+        self.assertIn("State." + state.id, self.storage.all())
+        self.assertIn("Place." + place.id, self.storage.all())
+        self.assertIn("city." + city.id, self.storage.all())
+        self.assertIn("Amenity." + amenity.id, self.storage.all())
+        self.assertIn("Review." + review.id, self.storage.all())
 
 
 if __name__ == '__main__':
